f069828c527727268f6fed39cbf1df83a7640313
Gansaikhan Shur <gansaikhanshur@gmail.com>
2021-11-15 13:50:12 -0600

    fix wav to wav logic

--- convert.sh
+++ convert.sh
@@ -13,7 +13,7 @@ if [[ $INPUT_FILE == *".ac3" ]]; then
 	ffmpeg -i $INPUT_FILE -vcodec copy -acodec pcm_s16le -ar $SAMPLING_FREQUENCY -ac $NUM_AUDIO_CHANNELS $DEST_FILE
 fi
 
-if [[ $INPUT_FILE == *".wav" ]]; then
+if [[ $INPUT_FILE == *".wav" ]] && [[ $DEST_FILE == *".wav" ]]; then
 	tmp_file=$INPUT_FILE.tmp.wav
 	ffmpeg -i $INPUT_FILE -ar $SAMPLING_FREQUENCY -ac $NUM_AUDIO_CHANNELS $tmp_file
 	rm $INPUT_FILE
