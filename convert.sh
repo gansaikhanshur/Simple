87226f5de657cc776406e6513f3677a777ee22b8
Gansaikhan Shur <gansaikhanshur@gmail.com>
2021-11-18 09:37:56 -0600

    supports wav convertion

--- convert.sh
+++ convert.sh
@@ -5,12 +5,12 @@ DEST_FILE=$2
 NUM_AUDIO_CHANNELS=$3	# mono, stereo, etc.
 SAMPLING_FREQUENCY=$4	# 8khz, 16khz,  etc.
 
-if [[ $INPUT_FILE == *".mp3" ]] || [[ $INPUT_FILE == *".m4a" ]]; then
-	ffmpeg -i $INPUT_FILE -ar $SAMPLING_FREQUENCY -ac $NUM_AUDIO_CHANNELS $DEST_FILE 
+if [[ $INPUT_FILE =~ \.(aac|aiff|pcm) ]] && [[ $DEST_FILE == *".wav" ]]; then
+	ffmpeg -f s16le -ac $NUM_AUDIO_CHANNELS -ar $SAMPLING_FREQUENCY -i $INPUT_FILE $DEST_FILE
 fi
 
-if [[ $INPUT_FILE == *".ac3" ]]; then
-	ffmpeg -i $INPUT_FILE -vcodec copy -acodec pcm_s16le -ar $SAMPLING_FREQUENCY -ac $NUM_AUDIO_CHANNELS $DEST_FILE
+if [[ $INPUT_FILE =~ \.(flac|m4a|mp3|ogg|wma) ]] && [[ $DEST_FILE == *".wav" ]]; then
+	ffmpeg -i $INPUT_FILE -acodec pcm_s16le -ac $NUM_AUDIO_CHANNELS -ar $SAMPLING_FREQUENCY $DEST_FILE
 fi
 
 if [[ $INPUT_FILE == *".wav" ]] && [[ $DEST_FILE == *".wav" ]]; then
