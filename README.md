27b763203fe82fe8653c8fd00b50d8f919bcbbce
Gansaikhan Shur <gansaikhanshur@gmail.com>
2022-01-19 15:44:32 -0600

    MP3 full support

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-Program Usage -> ./audio-conv directory 1 8000 WAV
+directory			  --> The Folder Containing Audio Files to be Converted
+AC (# of channels)	  --> 1, 2, etc.
+AR (rate)			  --> 8000, 16000, etc.
+OutputFormat		  --> WAV, MP3, etc
+
+Program Usage --> ./aconv directory AC AR OutputFormat
--- convert.sh
+++ convert.sh
@@ -21,3 +21,24 @@ if [[ $DEST_FILE == *".wav" ]]; then
 		mv $tmp_file $INPUT_FILE
 	fi
 fi
+
+if [[ $DEST_FILE == *".mp3" ]]; then
+
+	if [[ $INPUT_FILE =~ \.(aac|aiff|flac|m4a|wav|ogg|wma) ]]; then
+		ffmpeg -i $INPUT_FILE -ar $SAMPLING_FREQUENCY -ac $NUM_AUDIO_CHANNELS -acodec libmp3lame $DEST_FILE
+	fi
+
+	if [[ $INPUT_FILE == *".pcm" ]]; then
+		tmp_file=$INPUT_FILE.tmp.mp3
+		ffmpeg -ar 16000 -ac $NUM_AUDIO_CHANNELS -f s16le -i $INPUT_FILE -c:a libmp3lame $tmp_file
+		ffmpeg -i $tmp_file -ar $SAMPLING_FREQUENCY -ac $NUM_AUDIO_CHANNELS $DEST_FILE
+		rm $tmp_file
+	fi
+
+	if [[ $INPUT_FILE == *".mp3" ]]; then
+		tmp_file=$INPUT_FILE.tmp.mp3
+		ffmpeg -i $INPUT_FILE -ar $SAMPLING_FREQUENCY -ac $NUM_AUDIO_CHANNELS $tmp_file
+		rm $INPUT_FILE
+		mv $tmp_file $INPUT_FILE
+	fi
+fi
