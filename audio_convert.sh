d663d9f5c7df9fa57f17f8ecddccb35c3c3535a2
Gansaikhan Shur <gansaikhanshur@gmail.com>
2022-02-21 20:12:46 -0600

    finalized runner

---
+++ audio_convert.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# EXAMPLE USE CASE:
+# 1. Set below 4 configurations
+# 2. Run './audio_convert.sh/' or 'sh audio_convert.sh'
+
+INPUT_DIR=./input_folder/		# path to folder containing audio files
+OUTPUT_DIR=./output_folder/
+NUM_AUDIO_CHANNELS=1			# mono (1), stereo (2), etc.
+SAMPLING_FREQUENCY=8000			# 8000, 16000, etc.
+OUTPUT_FORMAT=WAV				# supports -> WAV, MP3, M4A, FLAC, WMA, AAC, PCM, AIFF, OGG
+
+g++ --std=c++17 scripts/init.cpp -o do-convert
+./do-convert $INPUT_DIR $OUTPUT_DIR $NUM_AUDIO_CHANNELS $SAMPLING_FREQUENCY $OUTPUT_FORMAT 
+rm do-convert
--- scripts/init.cpp
+++ scripts/init.cpp
@@ -4,8 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <map>
+#include <vector>
+#include <sstream>
 
-std::string getOutputFormat(std::string &s, std::string file_path){
+std::string getOutputFormat(std::string &s, std::string file_path, std::string output_path){
     std::string output = "";
 
     std::map<std::string, std::string> my_map = {
@@ -22,7 +24,12 @@ std::string getOutputFormat(std::string &s, std::string file_path){
 
     output = my_map.find(s)->second;
 
-    output = file_path.substr(0, file_path.find(".")) + output;
+	std::string base_filename = file_path.substr(file_path.find_last_of("/") + 1);
+	std::string::size_type const p(base_filename.find_last_of('.'));
+	std::string file_without_extension = base_filename.substr(0, p);
+
+	output = output_path + file_without_extension + output;
+
     return output;
 }
 
@@ -31,13 +38,14 @@ int main(int argc, char* argv[])
     std::string path = argv[1];
     for (const auto & entry : std::filesystem::directory_iterator(path))
 	{
-		std::string num_audio_channels = argv[2];
-		std::string sampling_freq = argv[3];
-		std::string output_audio_format = argv[4];
+		std::string output_path = argv[2];
+		std::string num_audio_channels = argv[3];
+		std::string sampling_freq = argv[4];
+		std::string output_audio_format = argv[5];
 
 		std::string input_file = entry.path();
-		std::string dest_file = getOutputFormat(output_audio_format, input_file);
-		std::string shell_command = "./convert.sh";
+		std::string dest_file = getOutputFormat(output_audio_format, input_file, output_path);
+		std::string shell_command = "./scripts/convert.sh";
 		shell_command = shell_command + " " + input_file + " " + dest_file + " " + num_audio_channels +
 			" " + sampling_freq;
 		std::system(shell_command.c_str());
